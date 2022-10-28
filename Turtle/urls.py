552c20289120e58211c548d529dcf2da1a95a8ea
jclee0109 <jclee0109@gist.ac.kr>
2022-10-29 07:19:05 +0900

    refactor(Turtle/urls.py): Clean unnecessary url

--- Turtle/urls.py
+++ Turtle/urls.py
@@ -40,7 +40,6 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/', include('rest_framework.urls')),
     path('api/accounts/', include('accountapp.urls')),
     path('api/profiles/', include('profileapp.urls')),
     path('api/', include("measurementapp.urls")),
